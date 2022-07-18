8c3501da9c96403d88c467f11d797680e83d7d6b
mallickraja1909 <mallickraja1909@gmail.com>
2022-07-18 17:39:07 +0530

    fakeshop
    update my project

---
+++ fakeshop-rjs/src/containers/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "../App.css";
+const Footer = () => {
+  return (
+    <div className='foot'>
+        <br/>
+        <h1 className='h1f'>Footer</h1>
+    </div>
+  )
+}
+
+export default Footer;
---
+++ fakeshop-rjs/src/containers/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="ui-fixed-menu">
+      <div className="uicontainercenter">
+        <br/>
+      <h2 className="h-2"> Welcome to FaKeShOp </h2>
+        <br/>
+
+       
+      </div>
+     
+    </div>
+  );
+};
+
+export default Header;
